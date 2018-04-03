d777601688ae04602e03f83d60a3088e687c7b23
Hendi Santika <hendisantika@gmail.com>
2018-04-04 06:04:30 +0700

    #8 - Add Constants

---
+++ src/main/java/com/hendisantika/maildemo/util/Constants.java
@@ -0,0 +1,49 @@
+package com.hendisantika.maildemo.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : mail-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/04/18
+ * Time: 06.03
+ * To change this template use File | Settings | File Templates.
+ */
+public class Constants {
+
+    /**
+     * The Constant BLANK.
+     */
+    public static final String BLANK = "";
+
+    /**
+     * The Constant COLON.
+     */
+    public static final String COLON = " : ";
+
+    /**
+     * The Constant DASH.
+     */
+    public static final String DASH = " - ";
+
+    /**
+     * The Constant KEY_TXNID.
+     */
+    public static final String KEY_TXNID = "TXNID";
+
+    /**
+     * The Constant XPATH_TXNID.
+     */
+    public static final String XPATH_TXNID = "//transcationId";
+
+    /**
+     * The Constant ENTRY.
+     */
+    public static final String ENTRY = "Entry";
+
+    /**
+     * The Constant EXIT.
+     */
+    public static final String EXIT = "Exit";
+}
